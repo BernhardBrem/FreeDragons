b1de3dfea704984182e88befddc5f6963118d0e9
Bernhard Brem <mail@bernhard-brem.de>
2021-06-06 15:49:20 +0200

    Now we have at least a map

---
+++ FreeDragons_Android/FreeDragons_Android/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Mapsui;
+using Mapsui.Widgets;
+
+namespace FreeDragons_Android
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var map = new Map
+            {
+                CRS = "EPSG:3857",
+                Transformation = new Mapsui.Projection.MinimalTransformation()
+            };
+
+            var tileLayer = Mapsui.Utilities.OpenStreetMap.CreateTileLayer();
+
+            map.Layers.Add(tileLayer);
+            map.Widgets.Add(new Mapsui.Widgets.ScaleBar.ScaleBarWidget(map) { TextAlignment = Mapsui.Widgets.Alignment.Center, HorizontalAlignment = Mapsui.Widgets.HorizontalAlignment.Left, VerticalAlignment = Mapsui.Widgets.VerticalAlignment.Bottom });
+
+            mapView.Map = map;
+
+        }
+    }
+}
