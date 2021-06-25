f2ddfd6878c2533cb06718e78c81f58a89d86ad5
Bernhard Brem <mail@bernhard-brem.de>
2021-06-26 00:20:58 +0200

    Evolution

---
+++ FreeDragons_Mobile/FreeDragons_Mobile/NewQuestView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FreeDragons_Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewQuestView : ContentView
+    {
+        public NewQuestView()
+        {
+            InitializeComponent();
+            OK.Clicked += ProcessOK;
+            Cancel.Clicked += ProcessCANCLE;
+            this.IsVisible = false;
+            
+        }
+
+        private void ProcessOK(object sender, EventArgs e)
+        {
+            this.IsVisible = false;
+        }
+
+        private void ProcessCANCLE(object sender, EventArgs e)
+        {
+            this.IsVisible = false;
+        }
+    }
+}
